a25ac9d729c6867d962ae4e68e4b475993e56a0b
Code Writter <codewritter.1@gmail.com>
2022-11-13 10:26:01 +0530

    Working on GUI interface

--- config.py
+++ config.py
@@ -3,11 +3,17 @@
 2. Don't make any kind of change in variables.
 """
 
-## MYSQL Connection
-host = " "
-user = " "
-password = " "
-database = " "
+# > MYSQL Connection
+host = "localhost"
+user = "root"
+password = "password"
+database = "student"
 
-## System
-clean_cmd = "cls"
+# > System
+operating_system = "Windows"
+
+# > User Profile
+
+name = "Admin"
+phone_number = "00 000000000"
+email = "admin@admin.com"
