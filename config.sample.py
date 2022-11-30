17b4741df710c561fbc49e18dbbeab722e3b5c06
Code Writter <codewritter.1@gmail.com>
2022-12-01 01:39:54 +0530

    Fixed New Record Function Error.

---
+++ config.sample.py
@@ -0,0 +1,19 @@
+"""
+1. Add or Remove '#' before line to activate or deactivate it.
+2. Don't make any kind of change in variables.
+"""
+
+# > Database Connection Details (MYSQL)
+host = "localhost"
+user = "root"
+password = "root"
+database = "database"
+
+# > System
+operating_system = "Windows"
+
+# > User Profile
+
+name = "Admin"
+phone_number = "00 000000000"
+email = "admin@admin.com"
--- main.py
+++ main.py
@@ -2,137 +2,148 @@ import etc._funct_ as fnct
 import tkinter as base_builder
 from colorama import Fore, Style
 import os
-
+from time import sleep
 try:
     # Body Base
+    fnct.logs("Program Started")
     base = base_builder.Tk()
-    base.geometry("1280x480")
+    base.geometry("13600x720")
     base.minsize(967, 554)
+    icon_image = base_builder.PhotoImage(file='image/logo.png')
+    base.iconphoto(True, icon_image)
     if "config.py" in os.listdir():
 
         base.title("Student Information System")
 
         # Header Frame
-        header_frm = base_builder.Frame(base)
+        header_frm = base_builder.Frame(base, relief='solid', borderwidth=0)
         header_frm.pack(side='top', anchor='nw', fill='x')
 
         # Display Frame
-        display_frm = base_builder.Frame(base, relief='solid', borderwidth=2, width=100)
-        display_frm.pack(side='right', anchor='n', fill='y', pady=10, padx=25)
+        display_frm = base_builder.Frame(base, relief='solid', borderwidth=0, width=80)
+        display_frm.pack(side='right', anchor='n', fill='y', pady=0, padx=2)
 
         # Menu Frame
         menu_frm = base_builder.Frame(base, borderwidth=2, relief='solid', padx=0)
-        menu_frm.pack(side='left', anchor='w', fill='y', pady=10, padx=5)
+        menu_frm.pack(side='left', anchor='w', fill='y', pady=10, padx=0)
 
         # Displayer Box
         scroll = base_builder.Scrollbar(display_frm)
         scroll.pack(side='right', fill='y')
-        displayer = base_builder.Text(display_frm, height=90, width=140, fg='green', relief='solid',
-                                      borderwidth=2, padx=5, pady=5, font=("algeria", 10, 'bold'),
+        displayer = base_builder.Text(display_frm, height=90, width=132, fg='green', relief='solid',
+                                      borderwidth=2, padx=4, pady=5,
                                       yscrollcommand=scroll.set)
-        displayer.pack(side='right', anchor='e', pady=10, padx=5, fill='both')
+        displayer.pack(side='right', anchor='e', pady=10, padx=0)
         scroll.config(command=displayer.yview)
 
+        # Option Frame
+        option_frm = base_builder.Frame(header_frm, relief='solid', borderwidth=0, width=1070, height=95)
+        option_frm.place(x=285, y=5)
 
         # ----------------------------------------------------------------------------------------------------------------------
 
         # Functions
+
+        open_box = lambda: displayer.config(state='normal')
+
+        close_box = lambda: displayer.config(state='disabled')
+
         def display_clear():
+            open_box()
             displayer.delete("1.0", "end")
 
+        def frame_clear(frame):
+            for width in frame.winfo_children():
+                width.destroy()
 
         def view_tables():
             """ List all the tables from sql database. """
+            from tabulate import tabulate
             fnct.logs("Clicked on view table.")
             display_clear()
+            frame_clear(option_frm)
+            open_box()
+
             def table_select():
+                open_box()
                 global table
                 table = choicevalue.get()
+                display_clear()
                 displayer.insert(base_builder.END, f"\n {table} selected successfully")
+                close_box()
                 fnct.logs("Table selected successfully.")
 
-            tables = fnct.dbs_agent("SHow tables;")
-            for data in tables:
-                for sdata in data:
-                    displayer.insert(base_builder.END, f" {sdata}\n")
+            tables = fnct.dbs_agent("show tables;")
+            displayer.insert(base_builder.END, f"{tabulate(tables, tablefmt='fancy_grid')}")
+            close_box()
             fnct.logs("Table displayed successfully")
-            base_builder.Label(header_frm, text="Select Record", font=(" ", 15, 'bold')).pack(side='top', anchor='ne',
-                                                                                              padx=5, pady=15)
+            base_builder.Label(option_frm, text="Select Record", font=(" ", 13, 'bold')).place(x=10, y=5)
             choicevalue = base_builder.StringVar()
-            base_builder.Entry(header_frm, textvariable=choicevalue, relief='solid', borderwidth=2).pack(side='right',
-                                                                                                         anchor='ne',
-                                                                                                         fill='x',
-                                                                                                         padx=10,
-                                                                                                         pady=2)
-            base_builder.Button(header_frm, text='Submit', relief='solid', fg='green', command=table_select).pack(
-                side='right', anchor='ne', padx=5)
+            base_builder.Entry(option_frm, textvariable=choicevalue, relief='solid', borderwidth=1).place(x=10, y=35)
+            base_builder.Button(option_frm, text='Submit', relief='solid', fg='green', command=table_select, padx=10, pady=2).place(x=145, y=25)
 
 
         def show_record():
-            """ Displays all the data of a record. """
-            fnct.logs("Record displayed successfully.")
-            from tabulate import tabulate
-            display_clear()
-            record = fnct.dbs_agent(f"select ID, name, roll_num, Phone_num, Address from {table};")
-            displayer.insert(base_builder.END,
-                             f"\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> {table} <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n")
-            displayer.insert(base_builder.END, f"\n")
+            open_box()
+            try:
+                """ Displays all the data of a record. """
+                from tabulate import tabulate
+                display_clear()
+                frame_clear(option_frm)
+                header = ["ID", "Name", "Roll No.", "Phone No.", "Address", "Mother Name", "Father Name", "DOB"]
+                record = fnct.dbs_agent(f"select * from {table};")
+                displayer.insert(base_builder.END, f'{tabulate(record, headers=header, tablefmt="fancy_grid")}')
+                close_box()
+                fnct.logs("Record displayed successfully.")
+            except Exception as e:
+                fnct.logs(f"Error -> {e}")
+                print(Fore.RED + "Error while displaying records. To know more checkout the logs.")
+                Style.RESET_ALL
 
         def new_record():
-            import tkinter as tki
+            frame_clear(option_frm)
             display_clear()
             def submit_rec():
-                pass
-                try:
-                    fnct.dbs_agent(
-                        f"insert into {table} (Name, Rol_num, phone_num, father_name, mother_name, address, dob) values('{name.get()}', {int(roll_num.get())}, {int(phone_num.get())}, '{father_name.get()}', '{mother_name.get()}', '{address.get()}', '{dob.get()}')")
-                    fnct.logs("Data Submitted successfully.")
-                    displayer.insert(base_builder.TOP, "Data Added to the record.")
-                    exit()
-                except Exception as e:
-                    print(Fore.Red + "Error in Submitting record. To know more check the logs.")
-                    fnct.logs(f"Error -> {e}")
-                    exit()
-            height = 280
-            width = 350
-            new_rec_base = tki.Tk()
-            new_rec_base.geometry(f"{width}x{height}")
-            new_rec_base.minsize(width, height)
-            new_rec_base.maxsize(width, height)
-            new_rec_base.title("Student Information System - New Record")
-            tki.Label(new_rec_base, text="NEW RECORD", fg="black", font=("Abel", 20, 'bold')).pack()
-            name = tki.StringVar
-            roll_num = tki.StringVar
-            phone_num = tki.StringVar
-            father_name = tki.StringVar
-            mother_name = tki.StringVar
-            address = tki.StringVar
-            dob = tki.StringVar
+                from tabulate import tabulate
+                open_box()
+                insert_agent = fnct.dbs_agent(f"insert into {table} (Name,Roll_num, phone_num, father_name, mother_name, address, dob) values('{name.get()}', '{roll_num.get()}', '{phone_num.get()}', '{father_name.get()}', '{mother_name.get()}', '{address.get()}', '{dob.get()}');")
+                fnct.logs("Data Recorded Successfully.")
+                header = ["ID", "Name", "Roll No.", "Phone No.", "Address", "Mother Name", "Father Name", "DOB"]
+                data_box =[[name.get(), roll_num.get(), phone_num.get(), father_name.get(), mother_name.get(), address.get(), dob.get()]]
+                displayer.insert(base_builder.END, f'{tabulate(data_box, headers=header, tablefmt="fancy_grid")}')
+                displayer.insert(base_builder.END, '\nData Recorded Successfully.\n')
 
-            tki.Label(new_rec_base, text="FULL NAME", font=(" ", 10, 'bold')).place(x=2, y=45)
-            tki.Entry(new_rec_base, textvariable=name, relief='solid').place(x=200, y=45)
+            name = base_builder.StringVar()
+            roll_num = base_builder.StringVar()
+            phone_num = base_builder.StringVar()
+            father_name = base_builder.StringVar()
+            mother_name = base_builder.StringVar()
+            address = base_builder.StringVar()
+            dob = base_builder.StringVar()
 
-            tki.Label(new_rec_base, text="ROLL NUMBER", font=(" ", 10, 'bold')).place(x=2, y=70)
-            tki.Entry(new_rec_base, textvariable=roll_num, relief='solid').place(x=200, y=70)
+            base_builder.Label(option_frm, text="FULL NAME : ", font=(" ", 10, 'bold')).place(x=5, y=2)
+            base_builder.Entry(option_frm, textvariable=name, relief='solid').place(x=95, y=4)
 
-            tki.Label(new_rec_base, text="PHONE NUMBER", font=(" ", 10, 'bold')).place(x=2, y=95)
-            tki.Entry(new_rec_base, textvariable=phone_num, relief='solid').place(x=200, y=95)
+            base_builder.Label(option_frm, text="ROLL NUMBER : ", font=(" ", 10, 'bold')).place(x=225, y=2)
+            base_builder.Entry(option_frm, textvariable=roll_num, relief='solid').place(x=335, y=4)
 
-            tki.Label(new_rec_base, text="FATHER NAME", font=(" ", 10, 'bold')).place(x=2, y=120)
-            tki.Entry(new_rec_base, textvariable=father_name, relief='solid').place(x=200, y=120)
+            base_builder.Label(option_frm, text="PHONE NUMBER : ", font=(" ", 10, 'bold')).place(x=460, y=2)
+            base_builder.Entry(option_frm, textvariable=phone_num, relief='solid').place(x=580, y=4)
 
-            tki.Label(new_rec_base, text="MOTHER NAME", font=(" ", 10, 'bold')).place(x=2, y=145)
-            tki.Entry(new_rec_base, textvariable=mother_name, relief='solid').place(x=200, y=145)
+            base_builder.Label(option_frm, text="FATHER NAME : ", font=(" ", 10, 'bold')).place(x=710, y=2)
+            base_builder.Entry(option_frm, textvariable=father_name, relief='solid').place(x=820, y=4)
 
-            tki.Label(new_rec_base, text="ADDRESS", font=(" ", 10, 'bold')).place(x=2, y=170)
-            tki.Entry(new_rec_base, textvariable=address, relief='solid').place(x=200, y=170)
+            base_builder.Label(option_frm, text="MOTHER NAME : ", font=(" ", 10, 'bold')).place(x=5, y=40)
+            base_builder.Entry(option_frm, textvariable=mother_name, relief='solid').place(x=115, y=42)
 
-            tki.Label(new_rec_base, text="D.O.B", font=(" ", 10, 'bold')).place(x=2, y=195)
-            tki.Entry(new_rec_base, textvariable=dob, relief='solid').place(x=200, y=195)
+            base_builder.Label(option_frm, text="ADDRESS : ", font=(" ", 10, 'bold')).place(x=250, y=40)
+            base_builder.Entry(option_frm, textvariable=address, relief='solid').place(x=325, y=42)
+            # #
+            base_builder.Label(option_frm, text="D.O.B : ", font=(" ", 10, 'bold')).place(x=460, y=40)
+            base_builder.Entry(option_frm, textvariable=dob, relief='solid').place(x=510, y=42)
 
-            tki.Button(new_rec_base, text='Submit', bg='#34a700', fg='white', padx=15, pady=5, font=(" ", 10, 'bold'), command=submit_rec).pack(side='bottom', pady=10)
-            new_rec_base.mainloop()
-
+            base_builder.Button(option_frm, text='Submit', bg='#34a700', fg='white', padx=15, pady=5, font=(" ", 10, 'bold'), command=submit_rec).place(x=950, y=2)
+        
         # Logo
         logo = base_builder.PhotoImage(file='image/logo.png')
         logo_printer = base_builder.Label(header_frm, image=logo, padx=5, pady=5)
@@ -161,7 +172,7 @@ try:
         del_rec.pack(pady=10)
 
         # Exit
-        ext = base_builder.Button(menu_frm, text="Exit", fg='green', borderwidth=1, relief='solid', pady=2, padx=45, font=("abel", 12, "bold"), command=exit)
+        ext = base_builder.Button(menu_frm, text="Exit", fg='green', borderwidth=1, relief='solid', pady=2, padx=45, font=("abel", 12, "bold"), command=quit)
         ext.pack(pady=10)
 
         # Creater Name
@@ -171,13 +182,14 @@ try:
 
     # Configuration
     else:
-        base.title("Student Information System - Configuration")
-        # Logo
-        logo = base_builder.PhotoImage(file='image/logo.png')
-        logo_printer = base_builder.Label(base, image=logo, padx=5, pady=5)
-        logo_printer.pack(side='left', anchor='nw', padx=20)
+        base.title("Student Information System - Configuration Error")
+        base.geometry("400x600")
+        base_builder.Label(base, text="Configuration file is missing.", fg='red', font=(' ', 20, 'bold')).pack(side='top')
+        base_builder.Button(base, text='Exit', relief='solid', borderwidth=1, pady=2, padx=5, command=quit).pack(side='bottom')
     base.mainloop()
 except Exception as error:
     fnct.logs(f"Error -> {error}")
     print(Fore.RED + "Error while executing the program. To know more checkout the logs.")
     Style.RESET_ALL
+    sleep(4)
+    exit()
